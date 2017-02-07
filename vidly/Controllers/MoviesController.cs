6f04b572ea46e09fcca4ab6ca596759f00b17df3
mansi.jain89 <mansi.jain89@gmail.com>
2017-02-07 18:32:20 +0530

    commiting the section one code.

---
+++ vidly/Controllers/MoviesController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using vidly.Models;
+
+namespace vidly.Controllers
+{
+    public class MoviesController : Controller
+    {
+        // GET: Movies
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Random()
+        {
+            var movie= new Movie(){Name = "Sherk",Id=1};
+
+            return View(movie);
+        }
+    }
+}
